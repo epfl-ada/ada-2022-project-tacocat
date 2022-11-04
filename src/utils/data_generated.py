53fd6e4a402812fcf40deeb0b3a3db844b9103fa
Jonas Konrad <jonas.konrad@mailbox.org>
2022-11-04 18:15:58 +0100

    combine cmu movie metadata with imdb data

--- src/utils/data_generated.py
+++ src/utils/data_generated.py
@@ -7,11 +7,20 @@ import pandas as pd
 PATH_DATA_GEN = '../generated/'
 
 FILENAME_ETHNICITIES = 'ethnicities'
+FILENAME_MOVIE_METADATA = 'movie_metadata'
 
 def load_ethnicities() -> pd.DataFrame:
     '''
     Load the ethnicities data.
 
-    :return: data in dataframe
+    :return: data in a dataframe
     '''
     return pd.read_pickle(PATH_DATA_GEN + FILENAME_ETHNICITIES)
+
+def load_movie_metadata() -> pd.DataFrame:
+    '''
+    Load the movie metadata data.
+
+    :return: data in a datafrmae
+    '''
+    return pd.read_pickle(PATH_DATA_GEN + FILENAME_MOVIE_METADATA)
